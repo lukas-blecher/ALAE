61ee53d8580a64e98f95176b68ee312f49ecc234
Lukas <luk.blecher@gmx.de>
2020-05-07 19:55:03 +0200

    add face classification model, and psnr
    noise to device (had problems with it)

--- net.py
+++ net.py
@@ -211,10 +211,10 @@ class DecodeBlock(nn.Module):
         if noise:
             if noise == 'batch_constant':
                 x = torch.addcmul(x, value=1.0, tensor1=self.noise_weight_1,
-                                  tensor2=torch.randn([1, 1, x.shape[2], x.shape[3]]))
+                                  tensor2=torch.randn([1, 1, x.shape[2], x.shape[3]], device=x.device))
             else:
                 x = torch.addcmul(x, value=1.0, tensor1=self.noise_weight_1,
-                                  tensor2=torch.randn([x.shape[0], 1, x.shape[2], x.shape[3]]))
+                                  tensor2=torch.randn([x.shape[0], 1, x.shape[2], x.shape[3]], device=x.device))
         else:
             s = math.pow(self.layer + 1, 0.5)
             x = x + s * torch.exp(-x * x / (2.0 * s * s)) / math.sqrt(2 * math.pi) * 0.8
@@ -231,10 +231,10 @@ class DecodeBlock(nn.Module):
         if noise:
             if noise == 'batch_constant':
                 x = torch.addcmul(x, value=1.0, tensor1=self.noise_weight_2,
-                                  tensor2=torch.randn([1, 1, x.shape[2], x.shape[3]]))
+                                  tensor2=torch.randn([1, 1, x.shape[2], x.shape[3]], device=x.device))
             else:
                 x = torch.addcmul(x, value=1.0, tensor1=self.noise_weight_2,
-                                  tensor2=torch.randn([x.shape[0], 1, x.shape[2], x.shape[3]]))
+                                  tensor2=torch.randn([x.shape[0], 1, x.shape[2], x.shape[3]], device=x.device))
         else:
             s = math.pow(self.layer + 1, 0.5)
             x = x +  s * torch.exp(-x * x / (2.0 * s * s)) / math.sqrt(2 * math.pi) * 0.8
@@ -307,7 +307,7 @@ class Encoder_old(nn.Module):
             mul *= 2
 
     def encode(self, x, lod):
-        styles = torch.zeros(x.shape[0], 1, self.latent_size)
+        styles = torch.zeros(x.shape[0], 1, self.latent_size, device=x.device)
 
         x = self.from_rgb[self.layer_count - lod - 1](x)
         x = F.leaky_relu(x, 0.2)
